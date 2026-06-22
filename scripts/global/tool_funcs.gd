285d4d95c77af5570d538eaa91e4d9a675bb0b54
Noah Klowden <chewie@nocturne.org>
2026-06-22 11:44:26 -0700

    Added some helper functions for set theory operations. Moved around the locations of the parameter scripts

--- scripts/global/tool_funcs.gd
+++ scripts/global/tool_funcs.gd
@@ -6,33 +6,33 @@ const INTERSECTION = "intersection"
 const DIFFERENCE = "difference"
 const UNION = "union"
 
-func _arr_comp_all(arr1, arr2):
-	var arr1_dict = {}
-	for val in arr1:
-		arr1_dict[val] = true
-	
+static func _arr_comp_all(arr1, arr2):
+	var arr2_dict = {}
+	for val in arr2:
+		arr2_dict[val] = true
+		
 	var intersection = []
 	var difference = []
 	var union = []
-	for val in arr2:
-		if arr1_dict.get(val, false):
+	for val in arr1:
+		if arr2_dict.get(val, false):
 			intersection.append(val)
 		else:
 			difference.append(val)
 	
-	union = arr2 + difference
+	union = arr1 + difference
 	
 	var collection = {
 		INTERSECTION: intersection,\
 		DIFFERENCE: difference,\
-		UNION: difference}
+		UNION: union}
 	
 	return collection
 
-func array_intersection(arr1, arr2):
+static func array_intersection(arr1, arr2):
 	return _arr_comp_all(arr1, arr2)[INTERSECTION]
 
-func array_difference(arr1, arr2):
+static func array_difference(arr1, arr2):
 	return _arr_comp_all(arr1, arr2)[DIFFERENCE]
 
 func array_union(arr1, arr2):
