fee2dc57b2e7a2478a5f1ff56a42a1e1523e58ba
Noah Klowden <chewie@nocturne.org>
2026-07-06 00:44:43 -0700

    Moved listener event over to events folder

--- scripts/resources/events/before_card_activated_event.gd
+++ scripts/resources/events/before_card_activated_event.gd
@@ -3,3 +3,9 @@ class_name BeforeCardActivatedEvent extends ListenerEvent
 '''
 Hello dave
 '''
+
+func _activate_event():
+	pass
+
+func _deactivate_event():
+	pass
--- scripts/resources/events/on_phase_event.gd
+++ scripts/resources/events/on_phase_event.gd
@@ -9,3 +9,9 @@ func _validate_property(property: Dictionary) -> void:
 		#Set range to only allow for phase values that aren't the base
 		property.hint_string = EventEnums.getEnumValsHintString(\
 		Enums.RoundPhase, Enums.RoundPhase.DRAW)
+
+func _activate_event():
+	pass
+
+func _deactivate_event():
+	pass
