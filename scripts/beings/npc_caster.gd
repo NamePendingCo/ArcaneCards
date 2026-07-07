268ef658cccb755d08157d26e376145c7cd2d4d2
Noah Klowden <chewie@nocturne.org>
2026-07-07 03:09:53 -0700

    Added NPC caster class and also test UI scene

---
+++ scripts/beings/npc_caster.gd
@@ -0,0 +1,12 @@
+class_name NPCCaster extends Caster
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _choose_being_from_range(param: BeingTargetParam):
+	pass
+
+func _choose_card_from_range(param: CardTargetParam):
+	pass
