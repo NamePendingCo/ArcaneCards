0336e28a04b6648367551625180d8688ea6bc1ff
Noah Klowden <chewie@nocturne.org>
2026-07-12 15:34:50 -0700

    Added the ability for the test UI to list options on the screen

--- scripts/misc/testing_battle_ui.gd
+++ scripts/misc/testing_battle_ui.gd
@@ -2,18 +2,19 @@ class_name TestingBattleUI extends CanvasLayer
 
 signal selection_made(selections)
 
-@onready var item_list: ItemList = $ItemList
+@onready var options_list: UIOptionsList = $OptionsList
 @onready var start_button: Button = $StartButton
 
 @export var battle_manager: BattleManager
 
-@export var acting_caster: PlayerCaster
+@export var acting_player: PlayerCaster:
+	set(val): set_acting_player(val)
 
-#Whichever player is the one being controlled
-var acting_player: Caster
+var active_decision: CasterDecision:
+	set(val): _set_active_decision(val)
 
 #List of every decision by each player
-var _decisions: Dictionary[Caster, CasterDecision]
+var _decisions: Dictionary[Caster, Array]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -37,9 +38,30 @@ func request_decision(caster: Caster, options: Array[String]):
 	var new_decision: CasterDecision = CasterDecision.new(caster, options)
 	
 	#Add decision to the list
-	_decisions[caster] = new_decision
+	_decisions[caster].append(new_decision)
+	
+	if caster == acting_player:
+		get_next_decision()
 	
 	return new_decision.decision_made #returns the signal so caster can wait for it
+
+'''
+Set whether or not the options list should be visible, allowing the player
+to make selections.
+'''
+func toggle_decision_mode(is_decision_time: bool):
+	options_list.visible = is_decision_time
+
+'''
+Sets the current acting player for the UI.
+'''
+func set_acting_player(caster: Caster):
+	acting_player = caster
+	get_next_decision()
+
+func get_next_decision():
+	if _decisions[acting_player].size() > 0:
+		active_decision = _decisions[acting_player][0]
 
 #================================================
 # Private methods
@@ -52,9 +74,18 @@ func _start_game():
 	else:
 		print("No battle manager found.")
 
+func _set_active_decision(decision: CasterDecision):
+	if active_decision != null:
+		options_list.multi_selected.disconnect(active_decision._handle_toggled_selection)
+	
+	active_decision = decision
+	options_list.multi_selected.connect(decision._handle_toggled_selection)
+	
+	_set_item_list_items(decision._options)
+
 func _set_item_list_items(items: Array[String]):
 	for item in items:
-		item_list.add_item(item)
+		options_list.add_item(item)
 
 '''Class that allows the UI to track multiple caster decisions at once, 
 and queue them up as necessary.
@@ -66,16 +97,31 @@ class CasterDecision:
 	var caster: Caster
 	
 	#the choices the caster can make
-	var options: Array[String]
+	var _options: Array[String]
+	var _selections: Array[int]
 	
-	func _init(deciding_caster: Caster, options_list: Array[String]):
+	func _init(deciding_caster: Caster, options: Array[String]):
 		caster = deciding_caster
-		options = options
+		_options = options
+		_selections = []
 	
 	'''
 	Makes the final decision for the caster decision
 	Params:
 		selections: a list of indices from the options that were chosen
 	'''
-	func make_decision(selections: Array[int]):
-		decision_made.emit(options, selections)
+	func make_decision():
+		decision_made.emit(_options, _selections)
+	
+	func _handle_toggled_selection(choice: int, selected: bool):
+		if selected:
+			_add_selection(choice)
+		else:
+			_remove_selection(choice)
+	
+	func _add_selection(choice: int):
+		if choice not in _selections:
+			_selections.append(choice)
+	
+	func _remove_selection(choice: int):
+		_selections.erase(choice)
---
+++ scripts/misc/ui_options_list.gd
@@ -0,0 +1,57 @@
+class_name UIOptionsList extends ItemList
+
+signal option_chosen(index)
+signal option_unchosen(index)
+
+@onready var ui_selected_items: ItemList = $SelectedItems
+
+#Two dictionaries for converting indices between each list
+var _selected_to_option: Dictionary[int, int] = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	multi_selected.connect(_on_item_toggled)
+	ui_selected_items.item_activated.connect(_unselect_via_selection_list)
+
+'''
+Params:
+	- Option index: the index from the option list toggled
+	- selected: whether it was selected or deselected
+'''
+func _on_item_toggled(option_index: int, selected: bool):	
+	if selected:
+		_add_ui_selected_item(option_index)
+	else:
+		_reset_ui_selected_list_items()
+
+'''
+Adds an item to the UI for selected items, as well as adds to the 
+conversion dictionary
+'''
+func _add_ui_selected_item(index):
+	ui_selected_items.add_item(get_item_text(index))
+	_selected_to_option[ui_selected_items.item_count]
+
+'''
+Reset the items in the selection_list
+'''
+func _reset_ui_selected_list_items():
+	var selections = get_selected_items()
+	
+	#reset list so it can be repopulated
+	ui_selected_items.clear()
+	
+	#reset mapping dictionary
+	_selected_to_option.clear()
+	
+	for index in selections:
+		_add_ui_selected_item(index)
+
+'''
+Allows for handling when the item in the selection list is double clicked
+'''
+func _unselect_via_selection_list(selection_index: int):
+	var option_index = _selected_to_option[selection_index]
+	
+	deselect(option_index)
+	multi_selected.emit(option_index, false)
