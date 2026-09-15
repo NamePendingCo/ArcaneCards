32597e1ed888596822a7a5bd1ef2b019b4dd98a0
Noah Klowden <chewie@nocturne.org>
2026-09-14 18:36:40 -0700

    Added range handling for int param

--- events/event/components/event_parameters/int_param/int_param.gd
+++ events/event/components/event_parameters/int_param/int_param.gd
@@ -1,11 +1,20 @@
 @abstract
 class_name IntParam extends EventParam
 
-@export
-var value: int
+## The primary value of the parameter.
+@export var value: int:
+	set(new_val): value = clamp(new_val, _min_val, _max_val)
 
-@export
-var value_dict: Dictionary[String, int]
+## Minimum value allowed for parameter.
+@export var _min_val: int:
+	set(new_val): _min_val = clamp(new_val, INT32_MIN, _max_val)
+
+## Maximum value allowed for parameter.
+@export var _max_val: int:
+	set(new_val): _max_val = clamp(new_val, _min_val, INT32_MAX)
+
+## A dictionary containing keyed values, if needed.
+@export var value_dict: Dictionary[String, int]
 
 ## Gets a value based on a given key (usually a target's id for an effect.)
 ## If the key does not have a value, return the base value instead.
@@ -13,4 +22,4 @@ func get_keyed_value(key: String):
 	return value_dict.get(key, value)
 
 func _set_keyed_value(key: String, val: int):
-	value_dict[key] = value
+	value_dict[key] = clamp(val, _min_val, _max_val)
--- events/event/components/event_parameters/int_param/int_resource.gd
+++ events/event/components/event_parameters/int_param/int_resource.gd
@@ -2,5 +2,14 @@
 @abstract
 class_name IntParamResource extends EventParamResource
 
-@export
-var default_value: int
+## Default value for the parameter.
+@export var default_value: int = 0:
+	set(new_val): default_value = clamp(new_val, _min_val, _max_val)
+
+## Minimum value allowed for parameter.
+@export var _min_val: int = INT32_MIN:
+	set(new_val): _min_val = clamp(new_val, INT32_MIN, _max_val)
+
+## A dictionary containing keyed values, if needed.
+@export var _max_val: int = INT32_MAX:
+	set(new_val): _max_val = clamp(new_val, _min_val, INT32_MAX)
