6dc1a8ca6f8e3bfb933a585ab1a3d777bd11ea18
Noah Klowden <chewie@nocturne.org>
2026-09-14 18:47:59 -0700

    Removed redundancy

--- events/event/components/event_parameters/int_param/int_op_param/int_op_param.gd
+++ events/event/components/event_parameters/int_param/int_op_param/int_op_param.gd
@@ -1,16 +1,21 @@
-class_name BasicIntResource extends IntParamResource
+class_name IntOpParamResource extends IntParamResource
 
 ## Stores data for a [BasicIntParam], a basic parameter for tracking int values.
 
+
+
 func build_param(actor: Actor, card: Card) -> EventParam:
-	var param = BasicIntParam.new_basic_int_param(actor, card, is_chosen, default_value, _min_val, _max_val)
+	var param = BasicIntParam.new_basic_int_param(actor, card, is_chosen, default_value, min_val, max_val)
 	
 	return param
 
-class BasicIntParam extends IntParam:
+class IntOpParam extends IntParam:
 	
 	## A basic parameter for tracking int values.
 	
+	var min_val: int
+	var max_val: int
+
 	static func new_basic_int_param(actor: Actor, card: Card, chosen: bool, 
 	default: int, min: int, max: int) -> BasicIntParam:
 		var param = BasicIntParam.new()
